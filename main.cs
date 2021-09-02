2c0a0f78898d01bdb7fed8517ab1eb61cde9e538
Jeff <jeckman03@gmail.com>
2021-09-02 22:22:25 +0000

    UI

--- main.cs
+++ main.cs
@@ -2,29 +2,87 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
+		
+		ConsoleHelper.Header();
+		Console.WriteLine("1. Create Routine");
+		Console.WriteLine("0. Exit Program");
+		string userSelecetion = Console.ReadLine();
+		int userSelecetionInt;
 
-		// Create new user UserAccountModel
-		UserAccountModel user = new UserAccountModel();
+		while(!int.TryParse(userSelecetion, out userSelecetionInt) || userSelecetionInt != 0 && userSelecetionInt != 1)
+		{
+			ConsoleHelper.Header();
+			Console.WriteLine("Invalid input. Please enter a 1 or 0 for your selsection:");
+			Console.WriteLine("1. Create Workout Routine");
+			Console.WriteLine("0. Exit Program");
+			userSelecetion = Console.ReadLine();
+		}
+
+		while(userSelecetionInt != 0)
+		{
+			// Create new user UserAccountModel
+			UserAccountModel user = new UserAccountModel();
+
+			// Naming and creating the new routine
+			ConsoleHelper.Header();
+			Console.WriteLine("-Creating new routine-");
+			Console.Write("What is the name of your routine: ");
+			string routineName = Console.ReadLine();
+
+			user.CreateNewRoutine(routineName);
+
+			// Display current routines	
+			ConsoleHelper.Header();
+			Console.WriteLine("New Routine Created! ");
+			Console.WriteLine();
+
+			user.SeeAllRoutines();
+
+			Console.WriteLine();
+			Console.WriteLine("Selections:");
+			Console.WriteLine("1. Create workout routine");
+			Console.WriteLine("2. Add Lifts to a created routine");
+			Console.WriteLine("0. Exit Program");
+			Console.WriteLine();
+			Console.Write("Please select an option: ");
+			userSelecetion = Console.ReadLine();
+
+			while(!int.TryParse(userSelecetion, out userSelecetionInt) |userSelecetionInt != 0 && userSelecetionInt != 1)
+			{
+				ConsoleHelper.Header();
+				user.SeeAllRoutines();
+				Console.WriteLine();
+				Console.WriteLine("Invalid input. Please enter a 1 or 0 for your selsection:");
+				Console.WriteLine("1. Create workout routine");
+				Console.WriteLine("2. Add Lifts to a created routine");
+				Console.WriteLine("0. Exit Program");
+				userSelecetion = Console.ReadLine();
+		}
+
+			Console.ReadLine();
+
+		}
+
+		Console.Clear();
+		Console.WriteLine("Have a great day!");
+		Console.ReadLine();
+
+
+
 
 		// Create a new routine
-		user.CreateNewRoutine("Chest Day");
-		user.CreateNewRoutine("Leg Day");
-		user.CreateNewRoutine("Back Day");
-
-		user.SeeAllRoutines();
-
-		Console.WriteLine();
-
-		user.SelectedRoutine(1).DisplayRoutine();
-
-
+		// user.CreateNewRoutine("Chest Day");
+		// user.CreateNewRoutine("Leg Day");
+		// user.CreateNewRoutine("Back Day");
 
 		// Add a few lifts
-			// routine.AddNewLift("Squat", 4, 6);
-			// routine.AddNewLift("Front Squat", 3, 6);
-			// routine.AddNewLift("Barbell Lunges", 2, 12);
+			// user.SelectedRoutine(1).AddNewLift("Squat", 4, 6);
+			// user.SelectedRoutine(1).AddNewLift("Front Squat", 3, 6);
+			// user.SelectedRoutine(1).AddNewLift("Barbell Lunges", 2, 12);
 
 		// Display routine
+		// user.SelectedRoutine(1).DisplayRoutine();
+
 		// ConsoleHelper.DisplayRoutine(routine);
 		
     Console.ReadLine();
@@ -34,6 +92,13 @@ class Program {
 
 public static class ConsoleHelper
 {
+	public static void Header()
+	{
+		Console.Clear();
+		Console.WriteLine("---CONSOLE WORKOUT APP---");
+		Console.WriteLine();
+	}
+
 	// See all created routines
 	public static void SeeAllRoutines(this UserAccountModel routines)
 	{
